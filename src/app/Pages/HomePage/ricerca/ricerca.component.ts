6a30fa310813277291cf040131f995f40bf86af9
Miliar <dario.bellini3@gmail.com>
2025-01-22 23:27:49 +0100

    Fix ricerca component

--- src/app/Pages/HomePage/ricerca/ricerca.component.ts
+++ src/app/Pages/HomePage/ricerca/ricerca.component.ts
@@ -76,7 +76,7 @@ export class RicercaComponent implements OnInit {
         this.listaAttivitaPerRicerca = this.attivitaService.GetListaAttivitaPerRicercaSession();
       if(!this.listaAttivitaPerRicerca || this.listaAttivitaPerRicerca.length == 0){
         try {
-          const listaAttivita = await lastValueFrom(this.attivitaService.apiGetListaAttivitaPerRicerca());
+          const listaAttivita = await lastValueFrom(this.attivitaService.apiGetListaAttivitaPreSearch());
           if (listaAttivita) {
               this.listaAttivitaPerRicerca = listaAttivita;
               this.attivitaService.createListaAttivitaPerRicercaSession(this.listaAttivitaPerRicerca);
