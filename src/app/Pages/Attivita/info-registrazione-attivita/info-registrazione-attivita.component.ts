fffbbf160afffb4bdfe8b44a9a1553e43a041d32
marco cangi <marcocangi@hotmail.it>
2025-08-14 22:03:15 +0200

    fix ios

--- src/app/Pages/Attivita/info-registrazione-attivita/info-registrazione-attivita.component.ts
+++ src/app/Pages/Attivita/info-registrazione-attivita/info-registrazione-attivita.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoRegistrazioneAttivitaComponent  implements OnInit {
 
+  features = [
+  { titleKey: 'FASTREGISTRATION',   descKey: 'VISIBLEONREADEAL',         img: 'assets/Img/one more blu.png',               alt: 'Registrazione veloce' },
+  { titleKey: 'CUSTOMPROMOTIONS',   descKey: 'OFFERDISCOUNTS',           img: 'assets/Img/promozioni_personalizzabili.png', alt: 'Promozioni personalizzabili' },
+  { titleKey: 'AUDIENCE',           descKey: 'CREATEPROMOTION',          img: 'assets/Img/target_mirato.png',               alt: 'Target mirato' },
+  { titleKey: 'NOCOMMISSIONS',      descKey: 'NOCOMMISSIONSDESCRIPTION', img: 'assets/Img/zerocommissioni_1.png',           alt: 'Zero commissioni' },
+];
+
   constructor() { }
 
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit() {}
+  ngOnInit() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
 }
